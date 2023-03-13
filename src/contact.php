f4df5f5bf249def8e47c07f8140e7492f6e7bd43
NatashaShiwen Naylor <natnaylor@etmlnet.local>
2023-03-13 11:12:44 +0100

    contact fini

---
+++ src/contact.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <title>Sport</title>
+    <meta charset="utf-8">
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap"
+    rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/header.css"/>
+    <link rel="stylesheet" href="gutim-master/gutim-master/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="gutim-master/gutim-master/css/style.css" type="text/css">
+</head>
+<?php include("includes/header.html");?> 
+
+<!-- Contact Section Begin -->
+<section class="contact-section spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="contact-form">
+                        <h4>dites-nous vos avis</h4>
+                        <form action="#">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <input type="text" placeholder="Votre nom">
+                                </div>
+                                <div class="col-lg-6">
+                                    <input type="text" placeholder="Email">
+                                </div>
+                                <div class="col-lg-12">
+                                    <textarea placeholder="Votre message"></textarea>
+                                    <button type="submit">Envoyer</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact Section End -->
+
+<?php include("includes/footer.html");?> 
